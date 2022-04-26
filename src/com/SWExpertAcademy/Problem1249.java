70b138095e6876941b3ced1f2f2bed1c04e2501d
bbaekddo <bbaekddo100@naver.com>
2022-04-26 22:07:05 +0900

    Prob 1249 실패

--- src/com/SWExpertAcademy/Problem1249.java
+++ src/com/SWExpertAcademy/Problem1249.java
@@ -1,9 +1,39 @@
+/*
 package com.SWExpertAcademy;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
+class TempWay implements Comparable<TempWay> {
+    private final int x;
+    private final int y;
+    private final int repairTime;
+
+    TempWay(int x, int y, int repairTime) {
+        this.x = x;
+        this.y = y;
+        this.repairTime = repairTime;
+    }
+
+    // getter
+    public int getX() {
+        return this.x;
+    }
+    public int getY() {
+        return this.y;
+    }
+    public int getRepairTime() {
+        return this.repairTime;
+    }
+
+    // compare 대상
+    @Override
+    public int compareTo(TempWay tempWay) {
+        return this.repairTime - tempWay.repairTime;
+    }
+}
 public class Problem1249 {
 
     // 최단 거리 변수 생성
@@ -15,11 +45,13 @@ public class Problem1249 {
                 new FileReader("/Users/dohyeonsmac/Desktop/Programming/Algorithms/testInput/problem1249.txt")
         );
 
-        /*
+        */
+/*
         // 온라인 제출용
         InputStreamReader inputStreamReader = new InputStreamReader(System.in);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        */
+        *//*
+
 
         // testCase 입력
         int testCase = Integer.parseInt(bufferedReader.readLine());
@@ -46,47 +78,8 @@ public class Problem1249 {
     }
 
     public static void getShortWay(int[][] map, int x, int y, int distance) {
-        // 목표지점 도달시 반환
-        if (x == (map[0].length - 1) && y == (map[0].length - 1)) {
-            if (distance <= shortestDistance) {
-                shortestDistance = distance;
-            }
-            return;
-        } else if (distance > shortestDistance) {
-            return;
-        }
 
-        int[] temps = new int[4];
-        temps[0] = 10000; temps[1] = 10000; temps[2] = 10000; temps[3] = 10000;
-        if ((x - 1) > 0) {
-            temps[0] = map[x - 1][y];
-        }
-        if ((x + 1) < map.length) {
-            temps[1] = map[x + 1][y];
-        }
-        if ((y - 1) > 0) {
-            temps[2] = map[x][y - 1];
-        }
-        if ((y + 1) < map.length) {
-            temps[3] = map[x][y + 1];
-        }
+        // 우선순위 큐 적용
 
-        for (int i = 3; i > 0; i--) {
-            if (temps[i] != 10000) {
-                distance += map[x][y];
-                int tempValue = map[x][y];
-                map[x][y] = 10000;
-
-                switch (i) {
-                    case 0 -> getShortWay(map, (x - 1), y, distance);
-                    case 1 -> getShortWay(map, (x + 1), y, distance);
-                    case 2 -> getShortWay(map, x, (y - 1), distance);
-                    case 3 -> getShortWay(map, x, (y + 1), distance);
-                }
-
-                distance -= tempValue;
-                map[x][y] = tempValue;
-            }
-        }
     }
-}
+}*/
